bd899608c4fa24ed9441c3ff69d5c9c93e8efee0
ThanhTran <transinhthanh1999@gmail.com>
2022-02-26 22:10:52 +0700

    firt start

---
+++ src/main/java/com/drjoy/priticeSpringBoot_MongoDb/PriticeSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.drjoy.priticeSpringBoot_MongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriticeSpringBootMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriticeSpringBootMongoDbApplication.class, args);
+	}
+
+}
